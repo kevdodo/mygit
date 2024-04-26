89a16ba8f15db65160cfd00343826978a5faef57
kevin <kdo@caltech.edu>
2024-04-26 16:54:57 -0700

    idk where my .git is

--- src/add.c
+++ src/add.c
@@ -6,6 +6,23 @@
 #include "object_io.h"
 #include "util.h"
 
+typedef struct index_header_t{
+    /* data */
+    char signature[4];
+    char version_num[4];
+    uint32_t num_entries[4];
+};
+
+
+typedef struct index_file_t
+{
+    /* data */
+    char signature[4];
+    char version_num[4];
+    uint32_t num_entries[4];
+};
+
+
 void add_files(const char **file_paths, size_t file_count)
 {
     (void)file_paths;
