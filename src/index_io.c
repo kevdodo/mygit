911d0284455d658472c6dcd39bf787da871eafe1
kevin <kdo@caltech.edu>
2024-05-06 20:22:13 -0700

    uh

--- src/index_io.c
+++ src/index_io.c
@@ -21,7 +21,7 @@
 /* Expected file signature for index files */
 const char DIRC_SIG[4] = {'D', 'I', 'R', 'C'};
 
-const char *INDEX_PATH = "temp_idx_file"; //".git/index"; //   
+const char *INDEX_PATH = ".git/index"; //"temp_idx_file"; //   
 
 /*
  * Read a big-endian bytestream
