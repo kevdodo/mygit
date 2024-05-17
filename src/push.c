ad169c9f2dfcde58276b8588db3a411f600b3921
kevin <kdo@caltech.edu>
2024-05-17 16:00:44 -0700

    sends on update

--- src/push.c
+++ src/push.c
@@ -198,14 +198,16 @@ hash_table_t *push_branches_for_remote(linked_list_t *branch_list, char *remote,
         char **hashes_to_push = get_commit_hashes_to_push(curr_hash, remote_hash);
 
         // TODO: Handle deleting the ref
-        char *old_hash = remote_hash;
-        for (size_t i=0; hashes_to_push[i] != NULL; i++){
-            printf("hash to push: %s\n", hashes_to_push[i]);
-            send_update(transport, ref, old_hash, hashes_to_push[i]);
-            old_hash = hashes_to_push[i];
-        }
+        // char *old_hash = remote_hash;
+        // for (size_t i=0; hashes_to_push[i] != NULL; i++){
+        //     printf("hash to push: %s\n", hashes_to_push[i]);
+        //     old_hash = hashes_to_push[i];
+        // }
 
-        finish_updates(transport);
+        // send one update for each branch
+        send_update(transport, ref, remote_hash, curr_hash);
+
+
 
         // TODO: Not efficient think of a better way????
         for (size_t i=0; hashes_to_push[i] != NULL; i++){
@@ -213,6 +215,8 @@ hash_table_t *push_branches_for_remote(linked_list_t *branch_list, char *remote,
         }
         branch = branch->next;
     }
+    finish_updates(transport);
+
     return hash_set;
 }
 
