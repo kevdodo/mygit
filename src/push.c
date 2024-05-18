862f734f1f35e7774136ccff5cc5632a85a42f97
kevin <kdo@caltech.edu>
2024-05-18 01:19:56 -0700

    o no

--- src/push.c
+++ src/push.c
@@ -388,7 +388,7 @@ void push(size_t branch_count, const char **branch_names, const char *set_remote
                 config = new_config;
             }
         }
-        free_config(config);
+        // free_config(config);
     }
 
     if (branch_count == 0 && branch_names == NULL){
