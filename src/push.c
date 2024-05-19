c4fc4b612540ae567192b80bc89ad8a2dff9e5c1
kevin <kdo@caltech.edu>
2024-05-19 15:12:43 -0700

    keep reading the commit

--- src/push.c
+++ src/push.c
@@ -354,6 +354,7 @@ void push(size_t branch_count, const char **branch_names, const char *set_remote
                 // while (true){
                 //     printf("aa\n");
                 // }
+                config = read_config();
                 continue;
             } 
             config_t *new_config = copy_config_and_add_section(config, branch_name, set_remote);
